3c54dd66c3f6f01bafa45a8f71f65963eef083e5
Prabhat Shukla <prabhatshukla9797@gmail.com>
2022-09-21 08:37:54 +0530

    code fixed by prabhat

--- prime.js
+++ prime.js
@@ -1,9 +1,18 @@
-function prime(num){
-    if(num%i){
+function prime(number){
+    let factors=0;
+    for(i=1;i<=number;i++){
+        if(number%i==0){
+            factors++;
+        }
+    }
+    if(factors==2){
         return true;
     }
+    return false;
 }
-return false;
+let ans=prime(17);
+if(ans==true){
+    console.log("Prime");
+}else{
+    console.log("Not Prime");
 }
-
-prime(13);
